064354bc70ae9b30594100bb72ab25abcf0f953f
Tyler Burton <tyler.burton@gsa.gov>
2025-06-03 14:59:06 -0500

    adds acme challenge for CE explorer

--- terraform/innovation.gov.tf
+++ terraform/innovation.gov.tf
@@ -70,6 +70,14 @@ resource "aws_route53_record" "permitting_innovation_gov_cname" {
   records = ["d1uq66d8ts87r6.cloudfront.net."]
 }
 
+resource "aws_route53_record" "acme_challenge_ce_permitting_innovation_gov_cname" {
+  zone_id = aws_route53_zone.innovation_toplevel.zone_id
+  name    = "_acme-challenge.ce.permitting.innovation.gov."
+  type    = "CNAME"
+  ttl     = 300
+  records = ["_acme-challenge.ce.permitting.innovation.gov.external-domains-production.cloud.gov."]
+}
+
 module "innovation_gov__email_security" {
   source  = "./email_security"
   zone_id = aws_route53_zone.innovation_toplevel.zone_id
